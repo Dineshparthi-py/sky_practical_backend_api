22a389f71ec3a27983718fd43827a5cf51a9367a
Dineshparthi-py <34937804+Dineshparthi-py@users.noreply.github.com>
2022-01-31 19:20:39 +0530

    project skeleton complete

---
+++ run.py
@@ -0,0 +1,17 @@
+from flask_cors import CORS
+import connexion
+
+
+def create_app(instance_config=None):
+    conn_app = connexion.FlaskApp(__name__, specification_dir='./app/openapi_v3')
+    app = conn_app.app
+    CORS(app)
+    conn_app.add_api('swagger_serialization.yml')
+    return conn_app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    # for development purpose configured single server(localhost) with debug
+    port = 5001
+    app.run(host='0.0.0.0', port=port, debug=True)
