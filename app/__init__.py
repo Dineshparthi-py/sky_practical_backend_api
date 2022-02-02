f4deca46b3e3e4e1f67b4d0e20a509c717fcb570
Dineshparthi-py <34937804+Dineshparthi-py@users.noreply.github.com>
2022-02-02 12:35:53 +0530

    validated auth added to both api, and bittrex url correction

--- app/__init__.py
+++ app/__init__.py
@@ -12,5 +12,5 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'sky_api_key')
 AUTH_TOKEN_EXPIRY_SECONDS = 14400
 
 # bittrex api config
-BITTREX_MARKET_SUMMARIES_API = "https://bittrex.github.io/api/v1-1/public/getmarketsummaries"
-BITTREX_MARKET_SUMMARY_API = "https://bittrex.github.io/api/v1-1/public/getmarketsummary"
+BITTREX_MARKET_SUMMARIES_API = "https://api.bittrex.com/api/v1.1/public/getmarketsummaries"
+BITTREX_MARKET_SUMMARY_API = "https://api.bittrex.com/api/v1.1/public/getmarketsummary"
